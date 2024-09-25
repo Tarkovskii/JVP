c2c1e53dd89c952451e380ef04f5b268b4a084da
nsundalov <nsundalov@mts.ru>
2024-09-26 00:07:09 +0300

    token fix, docs, envs, macen plugin

--- src/main/java/tutorial/Main.java
+++ src/main/java/tutorial/Main.java
@@ -1,19 +1,13 @@
 package tutorial;
 
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import tutorial.command.ListenerCommands;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
-
-        Bot bot = new Bot(/**for name and token*/);
+        String bot_name = System.getenv("BOT_NAME");
+        String token = System.getenv("BOT_TOKEN");
+        Bot bot = new Bot(bot_name, token);
         bot.connectionBot();
-
-
-
     }
 }
 
